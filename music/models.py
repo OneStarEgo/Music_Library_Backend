2d8d489c55a0d45729bf68b44242c94d22aeaa9b
Rodney McGee <rodneymcgee73@gmail.com>
2022-09-22 17:24:38 -0500

    Created Songs Model for Music App + Registered Music and Rest Framework in Project Settings Installed Apps Section

--- music/models.py
+++ music/models.py
@@ -1,3 +1,11 @@
+from platform import release
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title= models.CharField(max_length=255)
+    artist= models.CharField(max_length=255)
+    album= models.CharField(max_length=255)
+    release= models.DateField()
+    genre= models.CharField(max_length=255)
