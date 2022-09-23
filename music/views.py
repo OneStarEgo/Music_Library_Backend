34f20bf650a0ceac2c95cdac1a150e25abe98db2
Rodney McGee <rodneymcgee73@gmail.com>
2022-09-23 11:54:08 -0500

    Created GET, PUT, & DELETE Requests in Views.py

--- music/views.py
+++ music/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET', 'POST'])
@@ -16,3 +17,18 @@ def music_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
