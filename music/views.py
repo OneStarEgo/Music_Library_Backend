38051b30ac881e898e8cd0a5081d33c6d0494b4e
Rodney McGee <rodneymcgee73@gmail.com>
2022-09-23 11:03:44 -0500

    Created Post Request in Views.py

--- music/views.py
+++ music/views.py
@@ -2,11 +2,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Song
 from .serializers import SongSerializer
+from rest_framework import status
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def music_list(request):
-    songs = Song.objects.all()
-
-    serializer = SongSerializer(songs, many=True)
-
-    return Response(serializer.data)
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
